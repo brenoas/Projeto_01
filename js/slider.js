f12a044e034c21bf07d20485333ecd3cca0a3e80
Breno Arantes <b.arantes17@gmail.com>
2022-07-22 21:41:11 -0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,7 @@ $(function () {
 
     var curSlide = 0;
     var maxSlide = $('.banner-single').length - 1;
-    var delay = 4;
+    var delay = 3;
 
     
         initSlider();
@@ -14,6 +14,9 @@ $(function () {
         $('.banner-single').eq(0).show();
         for (var i = 0; i < maxSlide+1; i++) {
             var content = $('.bullets').html();
+            if (i == 0)
+            content+='<span class="active-slider"></span>'
+            else
             content+='<span></span>'
             $('.bullets').html(content);
         }
@@ -21,16 +24,26 @@ $(function () {
 
     function changeSlide() {
         setInterval(function() {
-            $('.banner-single').eq(curSlide).fadeOut(1000);
+            $('.banner-single').eq(curSlide).fadeOut(800);
             curSlide++;
             console.log(maxSlide);
             console.log(curSlide);
             if (curSlide > maxSlide)
             curSlide = 0;
-            $('.banner-single').eq(curSlide).fadeIn(1000, function(){
-
-            });
-        }, delay * 1000);
+            $('.banner-single').eq(curSlide).fadeIn(800);
+            //trocar bullets da nav do slider!
+            $('.bullets span').removeClass('active-slider');
+            $('.bullets span').eq(curSlide).addClass('active-slider');
+        }, delay * 5000);
     }
 
+
+    $('body').on('click','.bullets span', function(){
+        var currentBullet = $(this);
+        $('.banner-single').eq(curSlide).stop().fadeOut(800);
+        curSlide = currentBullet.index();
+        $('.banner-single').eq(curSlide).stop().fadeIn(800);
+        $('.bullets span').removeClass('active-slider');
+        currentBullet.addClass('active-slider');
+    })
 })
